d81b85c549094269a7bd1e708bc2362d45118b61
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2023-06-25 06:24:14 +0900

    Fix, Feat, Style: result style change, input fix & feat, plus comments #10

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,16 +1,49 @@
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import React from "react";
 
 const Result = ({ rounds, numbersList }) => {
+  const [showResult, setShowResult] = useState(false);
+
+  useEffect(() => {
+    if (rounds.length > 0 && numbersList.length === rounds.length) {
+      setShowResult(true);
+    }
+  }, [rounds, numbersList]);
+
+  const getBackgroundColor = (number) => {
+    if (number <= 10) {
+      return "#fbc400";
+    } else if (number <= 20) {
+      return "#69c8f2";
+    } else if (number <= 30) {
+      return "#ff7272";
+    } else if (number <= 40) {
+      return "#aaa";
+    } else {
+      return "#b0d840";
+    }
+  };
   return (
     <ReDiv>
-      <ul>
-        {numbersList.map((numbers, index) => (
-          <li key={index}>
-            {rounds[index] - 1}회차: {numbers.join(", ")}
-          </li>
-        ))}
-      </ul>
+      {showResult ? (
+        <ul style={{ listStyle: "none", lineHeight: "60px" }}>
+          {numbersList.map((numbers, index) => (
+            <li key={index}>
+              {rounds[index] - 1}회차:{" "}
+              {numbers.map((number, i) => (
+                <NumberSpan
+                  key={i}
+                  style={{ backgroundColor: getBackgroundColor(number) }}
+                >
+                  {number}
+                </NumberSpan>
+              ))}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <LoadingText>결과를 불러오는 중입니다...</LoadingText>
+      )}
     </ReDiv>
   );
 };
@@ -18,9 +51,47 @@ const Result = ({ rounds, numbersList }) => {
 export default Result;
 
 const ReDiv = styled.div`
+  overflow-y: scroll;
   width: 45%;
-  height: auto;
   margin-top: 1rem;
+  margin-bottom: 1rem;
   background-color: gray;
-  border-radius: 5px;
+  border-radius: 10px;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: beige;
+  }
+  ::-webkit-scrollbar-button {
+    width: 0;
+    height: 0;
+  }
+`;
+
+const NumberSpan = styled.span`
+  display: inline-block;
+  margin-top: 1rem;
+  margin-right: 0.5rem;
+  font-size: 1.5rem;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  text-align: center;
+  color: white;
+  text-shadow: 0px 0px 3px rgba(0, 49, 70, 0.8);
+`;
+
+const LoadingText = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: #888;
+  text-align: center;
+  padding: 20px;
 `;
